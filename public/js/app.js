e206b0028138a7cabc1a2373cea62911e6975bfb
unknown <muneebali500@gmail.com>
2020-12-29 11:33:58 +0500

    Remove unneccessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log(`hello how are`);
-
 const weatherForm = document.querySelector(`form`);
 const inputElement = document.querySelector(`input`);
 const messageOne = document.querySelector(`#message-1`);
